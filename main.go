b5e62a6febac8ffd18e3cb601fc13ce23c67a6f7
simon <ghdud0503@gmail.com>
2021-03-12 22:28:36 +0900

    echo server

--- main.go
+++ main.go
@@ -1,55 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"strconv"
+	"os"
+	"strings"
 
-	"github.com/PuerkitoBio/goquery"
+	"github.com/Ho-yeong/web-scrapper/scrapper"
+	"github.com/labstack/echo"
 )
 
-var baseUrl string = "https://kr.indeed.com/취업?q=node&vjk=3b61fa0edf9fe29e"
+var FileName = "jobs.csv"
+
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
+}
+
+func handleScrapper(c echo.Context) error {
+	defer os.Remove(FileName)
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	scrapper.Scrape(term)
+	return c.Attachment(FileName, FileName)
+}
 
 func main() {
-	totalPages := getPages()
-
-	for i := 0; i < totalPages; i++ {
-		getPage(i)
-	}
-}
-
-func getPage(page int) {
-	pageUrl := baseUrl + "&start=" + strconv.Itoa(page*50)
-	fmt.Println(pageUrl)
-}
-
-func getPages() int {
-	pages := 0
-	res, err := http.Get(baseUrl)
-	checkErr(err)
-	checkCode(res)
-
-	defer res.Body.Close()
-
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-	checkErr(err)
-
-	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
-		pages = s.Find("a").Length()
-	})
-
-	return pages
-}
-
-func checkErr(err error) {
-	if err != nil {
-		log.Fatalln(err)
-	}
-}
-
-func checkCode(res *http.Response) {
-	if res.StatusCode != 200 {
-		log.Fatalln("Request failed with status : ", res.StatusCode)
-	}
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrapper", handleScrapper)
+	e.Logger.Fatal(e.Start(":1323"))
 }
